5786887ad1f751adcedf2a82cc34405bedcae719
Preethi <iam.preethi.k@gmail.com>
2015-06-30 14:25:54 -0400

    06.30.2015 v4

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 // Problem: User when click on derived metric does nothing
 // Solution: When user clicks on derived metric, it should highlight the appropriate boxes that are related to the metric
 
-var $showARR = $('#ARR').click( function() {
-  $('#j_1').attr({ 'rect': { 'fill': '#00B88A'}})
-
+$('#ARR').click( function() {
+  $('#j_1').css("background-color", "fff");
 });
